731c63d58fef20cea3522072cae821914d4f2c75
SkyfallWasTaken <55807755+SkyfallWasTaken@users.noreply.github.com>
2023-12-31 09:05:06 +0000

    Update fonts.sh to error out

--- modules/bling/installers/fonts.sh
+++ modules/bling/installers/fonts.sh
@@ -3,4 +3,5 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-echo "!!!!! The bling font installer has been deprecated in favor of the fonts module. This error-free message will be removed in a future version. !!!!! "
+echo "!!!!! The bling font installer has been deprecated in favor of the fonts module. !!!!! "
+exit 1
